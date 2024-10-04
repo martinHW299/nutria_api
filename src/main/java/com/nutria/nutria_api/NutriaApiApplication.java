5a56dab539e716deecbc2097a3f5074c322e026e
martinHW299 <martin.hachiya299@gmail.com>
2024-10-03 20:42:23 -0400

    init project

---
+++ src/main/java/com/nutria/nutria_api/NutriaApiApplication.java
@@ -0,0 +1,13 @@
+package com.nutria.nutria_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nutria/nutria_api/NutriaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutria.nutria_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
